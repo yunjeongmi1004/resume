4578613e294a3fdc74ab15401c45795d15f88f96
yunjeongmi1004 <love_j90@naver.com>
2024-10-30 12:20:32 +0900

    배포설정 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomeView from "./views/HomeView";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<HomeView />} />
       </Routes>
